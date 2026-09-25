9969863bf7b8c96040dbe9377cce8e283c5ffd1c
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-09-25 12:55:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* =========================================================
    Service Worker — Gestão Patrimonial
 ========================================================= */
-const CACHE = 'gestao32m-v46';
+const CACHE = 'gestao32m-v47';
 const ASSETS = [
   './',
   './index.html',
